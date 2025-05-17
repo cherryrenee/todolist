2d10137cccaf5082c12162f5ecc8e6f5193e99f7
cherryrenee <sujinn1205@naver.com>
2025-05-17 20:30:43 +0900

    sdf
    sdf

--- components/imageFile.tsx
+++ components/imageFile.tsx
@@ -1,13 +1,31 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 export const FileUploader = () => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const fileRef = useRef<HTMLInputElement>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const isEnglishFileName = (name: string) => /^[a-zA-Z0-9_\-.]+$/.test(name);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-    if (file) {
-      console.log("Selected file:", file);
+    if (!file) return;
+    if(!isEnglishFileName(file.name)){
+      setError("파일 이름은 영어, 숫자, 밑줄, 하이픈으로만 이루어져있어야합니다.");
+      setImageUrl(null);
+      return;
     }
+    setError(null);
+    const reader=new FileReader();
+    reader.onload=()=>{
+      setImageUrl(reader.result as string)
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const openFileDialog = ()=> {
+    fileRef.current?.click();
   };
 
   return (
@@ -15,11 +33,25 @@ export const FileUploader = () => {
       <input
         type="file"
         accept="image/*"
-        ref={fileInputRef}
+        ref={fileRef}
         onChange={handleFileChange}
         style={{ display: "none" }}
       />
-      <button onClick={() => fileInputRef.current?.click()}>+</button>
+      {error && <p style={{color:"red"}}>{error}</p>}
+      {imageUrl===null ?( <div className="showImage"><Image src="/img.svg" alt="이미지파일" width={64} height={64} /><button onClick={openFileDialog} className="addImage"><Image src="/plusButton.svg" alt="빈 이미지 파일" width={24} height={24} /></button></div>): imageUrl && (
+        <div className="noBorder">
+          <img
+           src={imageUrl}
+           alt="미리보기"
+           className="showImages"
+           />
+          <button onClick={openFileDialog} className="editImage"><Image src="/edit.svg" alt="수정하기" width={24} height={24} /></button> 
+           </div>
+      )}
+      <div className="memo">
+        <input type="text" className="memoText"/>
+      </div>
     </div>
+    
   );
 };
