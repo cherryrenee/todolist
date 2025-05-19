a838f555d940b119d953d2b2c8294c97b6b2af07
cherryrenee <sujinn1205@naver.com>
2025-05-19 16:08:28 +0900

    이제 진ㅉ ㅏ 주석 전
    진짜임

--- app/detail/[todoDetail]/page.tsx
+++ app/detail/[todoDetail]/page.tsx
@@ -4,11 +4,25 @@ import { useParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import { FileUploader } from "@/components/imageFile";
 import { useTodoStore } from "@/store/todoStore";
+import { useState, useEffect } from "react";
 
 export default function DetailPage() {
   const params = useParams();
   const id = params.todoDetail as string;
   const router = useRouter();
+  const [largeScreen, setLaregeScreen] = useState<boolean>(false);
+  useEffect(() => {
+    const updateSize = () => {
+      setLaregeScreen(window.innerWidth >= 744);
+    };
+
+    updateSize(); // 초기 화면 크기 설정
+    window.addEventListener("resize", updateSize); // 리사이즈 이벤트 리스너 등록
+
+    return () => {
+      window.removeEventListener("resize", updateSize); // 컴포넌트 언마운트 시 리스너 제거
+    };
+  }, []);
 
   const { todos, toggleChecked, updateText } = useTodoStore();
   const todo = todos.find((t) => t.id === id);
@@ -17,15 +31,21 @@ export default function DetailPage() {
   return (
     <>
       <header>
-        <a href="/">
-          <Image
-            id="logo"
-            alt="logo"
-            src="/Size=Small.png"
-            width={71}
-            height={40}
-          />
-        </a>
+        <div className="doIt">
+          <a href="/">
+            {largeScreen ? (
+              <Image src="/largesize.svg" width={151} height={40} alt="logo" />
+            ) : (
+              <Image
+                id="logo"
+                alt="logo"
+                src="/smallsize.svg"
+                width={71}
+                height={40}
+              />
+            )}
+          </a>
+        </div>
       </header>
       <main>
         <div className="todoDiv">
